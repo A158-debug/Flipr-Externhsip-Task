358a1e04d7e69858cd843b12618451f067baf506
A158-debug <kumar.240@iitj.ac.in>
2023-06-24 10:10:59 +0530

    cors issue

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -7,7 +7,6 @@ export const getPosts = (page) => async (dispatch) => {
   try {
     dispatch({ type: "START_LOADING" });
     const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
-
     dispatch({ type: "FETCH_ALL", payload: { data, currentPage, numberOfPages } });
     dispatch({ type: "END_LOADING" });
   } catch (error) {
