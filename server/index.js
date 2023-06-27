f4be8f0235a39a2e2a3c9cc1be8ac22528d51343
A158-debug <kumar.240@iitj.ac.in>
2023-06-27 11:29:14 +0530

    cors issue

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '50mb', extended: true }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
-app.use(cors({ origin: '*' }))
+app.use(cors({ origin: '*', credentials: true, }))
 // app.use(cors({
 //     origin: (origin, callback) => {
 //       if (origin === 'http://localhost:3000' || origin === 'https://connectify-rho.vercel.app') {
