15931868fe5f336f5104071a088b4545f25f193f
A158-debug <kumar.240@iitj.ac.in>
2023-09-10 16:32:31 +0530

    production - make changes for production level

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://long-rose-betta-hat.cyclic.cloud/' });
-// const API = axios.create({ baseURL: 'http://localhost:5000/' });
+// const API = axios.create({ baseURL: 'https://long-rose-betta-hat.cyclic.cloud/' });
+const API = axios.create({ baseURL: 'http://localhost:8080/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
--- server/index.js
+++ server/index.js
@@ -7,26 +7,11 @@ import userRoutes from './routes/user.js'
 import postsRoutes from './routes/posts.js'
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json({ limit: '50mb', extended: true }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
-// app.use(cors())
-
-// app.use(cors({
-//     origin: "*"
-// }));
-
 app.use(cors({ origin: '*', credentials: true, }))
-// app.use(cors({
-//     origin: (origin, callback) => {
-//       if (origin === 'http://localhost:3000' || origin === '') {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     }
-//   }));
 
 
 app.use('/posts', postsRoutes)
@@ -36,8 +21,13 @@ app.get("/", (req, res) => {
     res.status(201).json({ message: "Connected to Backend!" });
 });
 
+// ! For production level
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
+})
+
 const CONNECTION_URL = process.env.MONGODB_DATABASE_URL
-// console.log(CONNECTION_URL)
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`App is Listening on PORT:${PORT}`)))
     .catch((error) => console.log(error))
