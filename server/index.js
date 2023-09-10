0f18abf3885a8dbc6d0cb31cbd919a3203268a64
A158-debug <kumar.240@iitj.ac.in>
2023-09-10 18:57:40 +0530

    fix code - for  production issue

--- server/index.js
+++ server/index.js
@@ -2,34 +2,38 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import path from 'path'
+import {fileURLToPath} from 'url';
 
 import 'dotenv/config'
 
 import userRoutes from './routes/user.js'
 import postsRoutes from './routes/posts.js'
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-console.log(__dirname)
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// console.log(__dirname)
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(express.json({ limit: '50mb', extended: true }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cors({ origin: '*', credentials: true, }))
 
 // ! For production level
-app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.use(express.static(path.join(__dirname, '../frontend/build', 'index.html')));
 
 app.use('/posts', postsRoutes)
 app.use('/user', userRoutes)
 
-// app.get("/", (req, res) => {
-//     res.status(201).json({ message: "Connected to Backend!" });
-// });
-
+app.get("/", (req, res) => {
+    res.status(201).json({ message: "Connected to Backend!" });
+});
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 })
 
 // const CONNECTION_URL = process.env.MONGODB_DATABASE_URL
